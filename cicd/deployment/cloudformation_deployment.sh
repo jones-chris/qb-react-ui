66a12f0d7a9bc904ca44b87258ebfe700a3a2fbb
Chris Jones <chris.jones@cfacorp.com>
2020-06-23 22:51:39 -0400

    Adding cloudformation templates and buildspec for CICD

---
+++ cicd/deployment/cloudformation_deployment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Find all the cloudformation templates.
+find ./cloudformation -name "*.cloudformation.yaml" > ./cf_stacks.txt
+
+while read line; do
+  # Get the index of the file (ex:  "0" if the file name is `0.codebuild_pipeline.cloudformation.yaml`.
+  INDEX=$(echo $line | awk -F . '{print $1}')
+
+  # The 1st index should be the codebuild pipeline cloudformation template (named, for example, `0.codebuild_pipeline.cloudformation.yaml`).
+  # If the file is the codebuild pipeline (the 1st index), then don't deploy because the pipeline should already exist and
+  # be running this shell script.
+  if [ $INDEX != "1" ]; then
+    NAME=$(echo $line | awk -F . '{print $2}')
+
+    aws cloudformation deploy --template-file $line --stack-name $NAME --capabilities CAPABILITY_NAMED_IAM
+  fi;
+
+done < ./cf_stacks.txt

8c66dc157ff796e86f52fdac23e7fda0b6c4f72b
Chris Jones <chris.jones@cfacorp.com>
2020-06-23 23:51:30 -0400

    Changed cloudformation_deployment.sh

--- cicd/deployment/cloudformation_deployment.sh
+++ cicd/deployment/cloudformation_deployment.sh
@@ -3,17 +3,24 @@
 # Find all the cloudformation templates.
 find ./cicd/cloudformation -name "*.cloudformation.yaml" > ./cf_stacks.txt
 
-while read line; do
+while read LINE; do
+  # Get file name.
+  FILE_NAME=$(basename "$LINE")
+
   # Get the index of the file (ex:  "0" if the file name is `0.codebuild_pipeline.cloudformation.yaml`.
-  INDEX=$(echo "$line" | awk -F . '{print $1}')
+  INDEX=$(echo "$FILE_NAME" | awk -F . '{print $1}')
+
+  echo "Index 1 after split is: $INDEX"
 
   # The 1st index should be the codebuild pipeline cloudformation template (named, for example, `0.codebuild_pipeline.cloudformation.yaml`).
   # If the file is the codebuild pipeline (the 1st index), then don't deploy because the pipeline should already exist and
   # be running this shell script.
-  if [ "$INDEX" != "1" ]; then
-    NAME=$(echo "$line" | awk -F . '{print $2}')
+  if [ "$INDEX" != "0" ]; then
+    NAME=$(echo "$FILE_NAME" | awk -F . '{print $2}')
 
-    aws cloudformation deploy --template-file "$line" --stack-name "$NAME" --capabilities CAPABILITY_NAMED_IAM
+    echo "Deploying $FILE_NAME with stack name, $NAME"
+
+    aws cloudformation deploy --template-file "$LINE" --stack-name "$NAME" --capabilities CAPABILITY_NAMED_IAM
   fi;
 
 done < ./cf_stacks.txt
